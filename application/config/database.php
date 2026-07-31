563465a6df3816d471302a88a26346a1fc1fe03d
Lian Permadi <lianpermadi1@gmail.com>
2026-07-31 11:27:35 +0700

    Perbarui pengaturan hostname dan tambahkan port untuk koneksi database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => getenv('IPAK_DB_HOST') !== false ? getenv('IPAK_DB_HOST') : 'localhost',
+	'hostname' => getenv('IPAK_DB_HOST') !== false ? getenv('IPAK_DB_HOST') : '127.0.0.1',
 	'username' => getenv('IPAK_DB_USER') !== false ? getenv('IPAK_DB_USER') : 'root',
 	'password' => getenv('IPAK_DB_PASSWORD') !== false ? getenv('IPAK_DB_PASSWORD') : '',
 	'database' => getenv('IPAK_DB_NAME') !== false ? getenv('IPAK_DB_NAME') : 'backoffice',
+	'port' => getenv('IPAK_DB_PORT') !== false ? getenv('IPAK_DB_PORT') : '3306',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
